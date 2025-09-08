0bfa328d82eb75b0e61a3d27c869d4546491cf0c
Redefyn-d <lokeshverma0609@gmail.com>
2025-09-08 15:54:08 +0530

    main page class implementation

--- data/cart.js
+++ data/cart.js
@@ -4,7 +4,7 @@ loadFromStorage();
 
 export function loadFromStorage(){
     cart = JSON.parse(localStorage.getItem('cart'));
-    if(!cart){
+    if(!cart || cart.length === 0){
         cart = [{
         productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
         quantity: 1,
--- data/products.js
+++ data/products.js
@@ -1,3 +1,6 @@
+import { moneyCal } from '../scripts/utils/money.js';
+
+
 export function getProduct (productId){
   let matchingProduct;    
       products.forEach((product)=>{
@@ -7,6 +10,48 @@ export function getProduct (productId){
       }); 
     return matchingProduct;
 }
+
+class Product{
+
+  constructor(productDetails){
+    this.id=productDetails.id; 
+    this.image=productDetails.image;
+    this.name=productDetails.name;
+    this.rating=productDetails.rating;
+    this.priceCents=productDetails.priceCents;
+    this.keywords=productDetails.keywords;
+
+  }
+
+  getStarsUrl(){
+    return `images/ratings/rating-${this.rating.stars*10}.png`;
+  }
+
+  getPrice(){
+    return `$${moneyCal(this.priceCents)}`;
+  }
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+}
+
+const product1 = new Product({
+  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+  rating: {
+    stars: 4.5,
+    count: 87
+  },
+  priceCents: 1090,
+  keywords: [
+    "socks",
+    "sports",
+    "apparel"
+  ]
+});
 
 export const products = [
   {
@@ -667,4 +712,7 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails)=>{
+  return new Product(productDetails);
+});
+console.log(products);
